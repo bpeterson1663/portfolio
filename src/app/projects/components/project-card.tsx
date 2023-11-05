da8ef4f953d994c7bba63cb2adbd17fdf802d044
Brady Peterson <bpeterson1663@gmail.com>
2023-11-05 14:04:13 -0600

    feat(prettier): ran prettier

--- src/app/projects/components/project-card.tsx
+++ src/app/projects/components/project-card.tsx
@@ -1,19 +1,25 @@
-import Image from "next/image";
-import Card from "@/app/components/card/card";
-import { Project } from "../model";
-import Link from "next/link";
+import Image from 'next/image'
+import Card from '@/app/components/card/card'
+import { Project } from '../model'
+import Link from 'next/link'
 import styles from '@/app/projects/components/project-card.module.scss'
 
-export function ProjectCard({title, description, imageUrl, projectRepo, url}: Project) {
-
-    return (
-        <Card className={styles['project-card']}>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <Image alt={title} src={imageUrl} width={200} height={200} />
-        {projectRepo && <Link className={styles.link} target="_blank" href={projectRepo}>Project Repo</Link>}
-        {url && <Link className={styles.link} target="_blank" href={url} title={title}>{title}</Link>}
-      </Card>
-    )
-
+export function ProjectCard({ title, description, imageUrl, projectRepo, url }: Project) {
+  return (
+    <Card className={styles['project-card']}>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <Image alt={title} src={imageUrl} width={200} height={200} />
+      {projectRepo && (
+        <Link className={styles.link} target="_blank" href={projectRepo}>
+          Project Repo
+        </Link>
+      )}
+      {url && (
+        <Link className={styles.link} target="_blank" href={url} title={title}>
+          {title}
+        </Link>
+      )}
+    </Card>
+  )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,24 +3,29 @@ import { Project } from './model'
 import { ProjectCard } from './components/project-card'
 
 export default function Page() {
-  const PROJECTS: Project[] = [{
-    id: "1",
-    title: "Wine Journal",
-    description: "An online journal to store your tasting notes about wine.",
-    imageUrl: "/static/wine-journal.png",
-    url: "https://wine-journal-711ef.web.app/"
-  }, {
-    id: "2",
-    title: "Pixl",
-    description: "A pixel art editor program created in Golang.",
-    imageUrl: "/static/pixl-art-editor.png",
-    projectRepo: "https://github.com/bpeterson1663/go-pixl",
-  }]
+  const PROJECTS: Project[] = [
+    {
+      id: '1',
+      title: 'Wine Journal',
+      description: 'An online journal to store your tasting notes about wine.',
+      imageUrl: '/static/wine-journal.png',
+      url: 'https://wine-journal-711ef.web.app/',
+    },
+    {
+      id: '2',
+      title: 'Pixl',
+      description: 'A pixel art editor program created in Golang.',
+      imageUrl: '/static/pixl-art-editor.png',
+      projectRepo: 'https://github.com/bpeterson1663/go-pixl',
+    },
+  ]
 
   return (
     <div>
       <Grid>
-        {PROJECTS.map(project => <ProjectCard key={project.id} {...project} /> )}
+        {PROJECTS.map((project) => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
       </Grid>
     </div>
   )
