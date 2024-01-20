f03425bfcbcaf2bc83aba9aa7620079108f3c81a
Brady Peterson <bpeterson1663@gmail.com>
2024-01-19 20:41:48 -0600

    feat(blog): add python web server post

--- src/app/blog/posts/index.tsx
+++ src/app/blog/posts/index.tsx
@@ -5,6 +5,7 @@ import P20240103TypescriptWeatherUI from './development/2024-01-03-Typescript-We
 import P20240104TypescriptWeatherUI from './development/2024-01-04-Typescript-Weather-UI'
 import P20240105TypescriptWeatherUI from './development/2024-01-05-Typescript-Weather-UI'
 import P20240110PythonWebScraper from './development/2024-01-10-Ptyhon-Web-Scraper'
+import P20240119PythonWebServer from './development/2024-01-19-Python-Web-Server'
 
 import P20240102Workout from './fitness/2024-01-02-Workout'
 import P20231229Workout from './fitness/2023-12-29-Workout'
@@ -15,8 +16,20 @@ import P20240105Workout from './fitness/2024-01-05-Workout'
 import P20240106Workout from './fitness/2024-01-06-Workout'
 import P20240108Workout from './fitness/2024-01-08-Workout'
 
-export const posts = [
+type Category = 'fitness' | 'development'
+
+interface Post {
+  category: Category
+  slug: string
+  title: string
+  date: Date
+  summary: string
+  page: JSX.Element
+}
+
+export const posts: Post[] = [
   {
+    category: 'fitness',
     slug: '2023-12-29-workout',
     title: 'Workout',
     date: new Date('December 29, 2023'),
@@ -24,6 +37,7 @@ export const posts = [
     page: <P20231229Workout />,
   },
   {
+    category: 'fitness',
     slug: '2023-12-30-workout',
     title: 'Workout',
     date: new Date('December 30, 2023'),
@@ -31,6 +45,7 @@ export const posts = [
     page: <P20231230Workout />,
   },
   {
+    category: 'development',
     slug: '2023-12-31-100-commits',
     title: 'Road to 100 Commits',
     date: new Date('December 31, 2023'),
@@ -38,6 +53,7 @@ export const posts = [
     page: <P20231231RoadTo100 />,
   },
   {
+    category: 'development',
     slug: '2024-01-01-typescript-weather',
     title: 'Typescript Weather Project',
     date: new Date('January 01, 2024'),
@@ -45,6 +61,7 @@ export const posts = [
     page: <P20240101TypescriptWeather />,
   },
   {
+    category: 'development',
     slug: '2024-01-02-typescript-weather',
     title: 'Finishing Weather CLI Project',
     date: new Date('January 02, 2024'),
@@ -52,6 +69,7 @@ export const posts = [
     page: <P20240102TypescriptWeather />,
   },
   {
+    category: 'fitness',
     slug: '2024-01-02-workout',
     title: 'Workout',
     date: new Date('January 02, 2024'),
@@ -59,6 +77,7 @@ export const posts = [
     page: <P20240102Workout />,
   },
   {
+    category: 'development',
     slug: '2024-01-03-typescript-weather-ui',
     title: 'Typescript Weather UI',
     date: new Date('January 03, 2024'),
@@ -66,6 +85,7 @@ export const posts = [
     page: <P20240103TypescriptWeatherUI />,
   },
   {
+    category: 'fitness',
     slug: '2024-01-03-workout',
     title: 'Workout',
     date: new Date('January 03, 2024'),
@@ -73,6 +93,7 @@ export const posts = [
     page: <P20240103Workout />,
   },
   {
+    category: 'fitness',
     slug: '2024-01-04-workout',
     title: 'Workout',
     date: new Date('January 04, 2024'),
@@ -80,6 +101,7 @@ export const posts = [
     page: <P20240104Workout />,
   },
   {
+    category: 'development',
     slug: '2024-01-04-typescript-weather-ui',
     title: 'Typescript Weather UI',
     date: new Date('January 04, 2024'),
@@ -87,6 +109,7 @@ export const posts = [
     page: <P20240104TypescriptWeatherUI />,
   },
   {
+    category: 'fitness',
     slug: '2024-01-05-workout',
     title: 'Workout',
     date: new Date('January 05, 2024'),
@@ -94,6 +117,7 @@ export const posts = [
     page: <P20240105Workout />,
   },
   {
+    category: 'development',
     slug: '2024-01-05-typescript-weather-ui',
     title: 'Typescript Weather UI',
     date: new Date('January 05, 2024'),
@@ -101,6 +125,7 @@ export const posts = [
     page: <P20240105TypescriptWeatherUI />,
   },
   {
+    category: 'fitness',
     slug: '2024-01-05-workout',
     title: 'Workout',
     date: new Date('January 06, 2024'),
@@ -108,6 +133,7 @@ export const posts = [
     page: <P20240106Workout />,
   },
   {
+    category: 'fitness',
     slug: '2024-01-08-workout',
     title: 'Workout',
     date: new Date('January 08, 2024'),
@@ -115,10 +141,21 @@ export const posts = [
     page: <P20240108Workout />,
   },
   {
+    category: 'development',
     slug: '2024-01-10-python-web-scraper',
-    title: "Python - Web Scraper",
+    title: 'Python - Web Scraper',
     date: new Date('January 10, 2024'),
-    summary: "Working on a web scraper of Hacker News",
-    page: <P20240110PythonWebScraper />
-  }
-].sort((a, b) => b.date.getTime() - a.date.getTime())
+    summary: 'Working on a web scraper of Hacker News',
+    page: <P20240110PythonWebScraper />,
+  },
+  {
+    category: 'development',
+    slug: '2024-01-19-python-web-server',
+    title: 'Python Web Server',
+    date: new Date('January 19, 2024'),
+    summary: 'Web Server using Python and hosted on Python Anywhere',
+    page: <P20240119PythonWebServer />,
+  },
+]
+
+export const postsSortedByDate: Post[] = posts.sort((a, b) => b.date.getTime() - a.date.getTime())
