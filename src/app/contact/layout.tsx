5758f06c77e54a0581d5db73d0bb1af2e03c317d
Brady Peterson <bpeterson1663@gmail.com>
2024-01-23 11:02:18 -0600

    feat(contact): build out framework for contact page

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react";
+import PageLayout from "../components/page-layout/page-layout";
+
+export default function ContactLayout({children}: PropsWithChildren) {
+    return (
+        <PageLayout title="Contact Me" subtitle="Send me a message">
+            {children}
+        </PageLayout>
+    )
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,28 @@
+export default function Page() {
+    return (
+        <form>
+            <label htmlFor="name">Your Name</label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+            />
+            
+            <label htmlFor="email">Your Email</label>
+            <input
+                type="email"
+                name="email"
+                id="email"
+            />
+            
+            <label htmlFor="message">Message</label>
+            <input
+                type="text"
+                name="message"
+                id="message"
+            />
+        </form>
+    )
+}
+    
+    
