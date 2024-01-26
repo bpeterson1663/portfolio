c1598f9d4cbe3292c8598b569f7432c4d37961f4
Brady Peterson <bpeterson1663@gmail.com>
2024-01-26 11:06:53 -0600

    feat(prettier): ran prettier

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -1,10 +1,10 @@
-import { PropsWithChildren } from "react";
-import PageLayout from "../components/page-layout/page-layout";
+import { PropsWithChildren } from 'react'
+import PageLayout from '../components/page-layout/page-layout'
 
-export default function ContactLayout({children}: PropsWithChildren) {
-    return (
-        <PageLayout title="Contact Me" subtitle="Send me a message">
-            {children}
-        </PageLayout>
-    )
+export default function ContactLayout({ children }: PropsWithChildren) {
+  return (
+    <PageLayout title="Contact Me" subtitle="Send me a message">
+      {children}
+    </PageLayout>
+  )
 }
